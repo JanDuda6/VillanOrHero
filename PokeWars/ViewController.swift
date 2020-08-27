6de02625048c5d378cb07dc20723c731b2c093e8
Jan Duda <jan.duda6@gmail.com>
2020-08-27 09:22:10 +0200

    Initial Commit

---
+++ PokeWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeWars
+//
+//  Created by Kurs on 27/08/2020.
+//  Copyright © 2020 Kurs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
