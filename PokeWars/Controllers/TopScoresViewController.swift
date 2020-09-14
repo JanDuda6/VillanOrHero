af40f670226bd7b8edb2d624d869eb4933c890bb
Jan Duda <jan.duda6@gmail.com>
2020-09-14 18:32:43 +0200

    userDefaults

--- PokeWars/Controllers/TopScoresViewController.swift
+++ PokeWars/Controllers/TopScoresViewController.swift
@@ -15,9 +15,27 @@ class TopScoresViewController: UIViewController {
     let questionManager = QuestionManager.sharedInstance
 
     override func viewDidLoad() {
+        if let topScore = questionManager.loadUserDefault()?.sorted(by: {$0.1 < $1.1}) {
+            for scores in topScore {
+                createLabel(with: "\(scores.key)    \(scores.value)")}
+
+        } else {
+
+            createLabel(with: "Empty")
+        }
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
+    }
+
+    func createLabel(with text: String) {
+        let targetLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        targetLabel.backgroundColor = .white
+        targetLabel.layer.masksToBounds = true
+        targetLabel.layer.cornerRadius = 5
+        targetLabel.text = text
+        targetLabel.textAlignment = .center
+        self.view.addSubview(targetLabel)
     }
 }
--- PokeWars/Model/QuestionManager.swift
+++ PokeWars/Model/QuestionManager.swift
@@ -10,9 +10,9 @@ import Foundation
 
 class QuestionManager {
     static let sharedInstance = QuestionManager()
-    let defaults = UserDefaults.standard
     private init() {}
 
+    let defaults = UserDefaults.standard
     var userResult = 0
     var questionNumber = 1
     var userName = ""
@@ -28,30 +28,31 @@ class QuestionManager {
     func addScoreToUserDefault(with key: String, and value: Int) {
 
         // UserDefaults array
-        if var userDefaultsArray = defaults.dictionary(forKey: "TopScore") {
-            let topScoreSorted = userDefaultsArray.sorted { $0.1 < $1.1 }
+        if let userDefaultsArray = defaults.dictionary(forKey: "TopScore") {
+
+            // convert [String: Any] to [String: Int]
+            var convertedDict = convertToStringIntDictionary(dictionary: userDefaultsArray)
+
+            // sort by value
+            let topScoreSorted = convertedDict.sorted {$0.1 < $1.1}
 
             // checks if name exists in userDefault array and if value is higher than in origin
-            for n in 0...3 {
-
+            for n in 0...topScoreSorted.count - 1 {
                 if topScoreSorted[n].key == key && topScoreSorted[n].value <= value {
-                    userDefaultsArray.removeValue(forKey: topScoreSorted[n].key)
-                    userDefaultsArray[key] = value
+                    convertedDict.removeValue(forKey: topScoreSorted[n].key)
+                    convertedDict[key] = value
                 }
             }
 
             // check if array has more than 5 elements and if value is higher than origin, if true removes last value and add higher one
             if topScoreSorted.count >= 5 && topScoreSorted[0].value <= value {
-                userDefaultsArray.removeValue(forKey: topScoreSorted[0].key)
-                userDefaultsArray[key] = value
+                convertedDict.removeValue(forKey: topScoreSorted[0].key)
+                convertedDict[key] = value
 
             } else if topScoreSorted.count < 5 {
-                userDefaultsArray[key] = value
+                convertedDict[key] = value
             }
-
-            let sortedArray = userDefaultsArray.sorted { $0.1 < $1.1 }
-
-            defaults.set(userDefaultsArray, forKey: "TopScore")
+            defaults.set(convertedDict, forKey: "TopScore")
 
         } else {
             let q = [key: value]
@@ -59,18 +60,27 @@ class QuestionManager {
         }
     }
 
+    func convertToStringIntDictionary(dictionary: [String: Any]) -> [String: Int] {
+
+        var convertedDictionary = [String: Int]()
+
+        for elements in dictionary {
+            convertedDictionary[elements.key] = elements.value as? Int
+        }
+        return convertedDictionary
+    }
+
+    func loadUserDefault() -> [String: Int]? {
+        if let userDefaultsArray = defaults.dictionary(forKey: "TopScore") {
+            return convertToStringIntDictionary(dictionary: userDefaultsArray)
+        } else {
+            return nil
+        }
+    }
+
     func clearQuestionManager() {
-        userResult = 0
-        questionNumber = 1
-        userName = ""
-    }
-
-    func sort(dic dic: [String: Int]) -> [String: Int] {
-        
-
-    }
-
-
-
-
+           userResult = 0
+           questionNumber = 1
+           userName = ""
+       }
 }
